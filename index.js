be8c6828b10c6c2bff549add3176e14c2ee835d9
Samuel-Adeyeye <samueladeyeye2012@gmail.com>
2023-07-22 21:57:12 +0100

    updated file

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ dotenv.config()
 const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
 
-// const MONGODB = "mongodb+srv://samuel:silentwords@graphql.2szucxi.mongodb.net/graphql?retryWrites=true&w=majority";
-
 /*Apollo Server requires two different parameters to run the server:
 typeDefs: GraphQL Type Definition.
 resolvers: How do we resolve queries / mutations?*/
